b04c2a01f2c395d07de303d506941ac94eedd5f3
269460 <269460@student.pwr.edu.pl>
2024-04-23 14:16:23 +0200

    plik

--- mining_summary/spiders/mining_summary.py
+++ mining_summary/spiders/mining_summary.py
@@ -61,3 +61,60 @@ if __name__ == "__main__":
     process = CrawlerProcess()
     process.crawl(MiningSpider)
     process.start()
+# 
+# import scrapy
+# from scrapy.crawler import CrawlerProcess
+# from sumy.parsers.plaintext import PlaintextParser
+# from sumy.nlp.tokenizers import Tokenizer
+# from sumy.summarizers.lsa import LsaSummarizer
+# import nltk
+# import html
+# import re
+# 
+# nltk.download('punkt')
+# 
+# from scrapy.utils.project import get_project_settings  # Import do użycia ustawień projektu
+# class MiningSpider(scrapy.Spider):
+#     name = "mining_summary"
+# 
+#     def start_requests(self):
+#         # Otwórz plik 'links.txt' i wczytaj URL-e
+#         with open('links.txt', 'r') as file:
+#             links = [url.strip() for url in file.readlines()]
+#             for url in links:
+#                 yield scrapy.Request(url=url, callback=self.parse)
+# 
+#                 
+#     def parse(self, response):
+#             self.logger.info(f"Przetwarzanie: {response.url}")
+#             if "koreabizwire.com" in response.url:
+#                 page_content = response.xpath('//div[@class="entry-content"]//p//text()').getall()
+#             else:
+#                 page_content = response.xpath('//p//text()').getall()
+#             if not page_content:
+#                 self.logger.error(f"Nie wyekstrahowano treści z: {response.url}")
+#             else:
+#                 self.logger.info(f"Ekstrahowana treść: {page_content[:100]}")
+#             content = ' '.join(page_content)
+#             cleaned_content = self.clean_text(content)
+#             summary = self.generate_summary(cleaned_content)
+#             filename = f"{response.url.split('/')[-2]}.txt"
+#             with open(filename, 'w', encoding='utf-8') as f:
+#                 f.write(summary)
+# 
+#     def clean_text(self, text):
+#             text = html.unescape(text)  # Dekoduje encje HTML
+#             text = re.sub(r'\s+', ' ', text)  # Usuwa nadmiarowe białe znaki
+#             return text.strip()
+# 
+#     def generate_summary(self, text, sentences_count=3):
+#             parser = PlaintextParser.from_string(text, Tokenizer("english"))
+#             summarizer = LsaSummarizer()
+#             summary = summarizer(parser.document, sentences_count)
+#             return ' '.join([str(sentence) for sentence in summary])
+# 
+#     # Uruchomienie pająka (dla celów przykładowych, zazwyczaj używa się 'scrapy crawl' z terminala)
+# if __name__ == "__main__":
+#     process = CrawlerProcess()
+#     process.crawl(MiningSpider)
+#     process.start()
--- mining_summary/spiders/model.py
+++ mining_summary/spiders/model.py
@@ -1,48 +1,56 @@
-# #Note: The openai-python library support for Azure OpenAI is in preview.
-#       #Note: This code sample requires OpenAI Python library version 1.0.0 or higher.
-# import os
-# from openai import AzureOpenAI
-#
-#
-# client = AzureOpenAI(
-#   #azure_endpoint = "https://ainewsletter.openai.azure.com/",
-#   api_key=os.getenv("ca920090faef4b558edfd2359d3488e3"),
-#   api_version="2024-02-15-preview",
-#   azure_endpoint = os.getenv("https://ainewsletter.openai.azure.com/")
-# )
-#
-#
-# def generate_openai_completion(prompt):
-#   message_text = [
-#     {"role": "system", "content": "This is a text to be summarized."},
-#     {"role": "user", "content": prompt}
-#   ]
-#
-#   completion = client.chat.completions.create(
-#     model="gpt-3.5-turbo",
-#     messages=message_text,
-#     temperature=0.7,
-#     max_tokens=800,
-#     top_p=0.95,
-#     frequency_penalty=0,
-#     presence_penalty=0,
-#     stop=None
-#   )
-#   return completion.choices[0].message['content']
-
+#Note: The openai-python library support for Azure OpenAI is in preview.
+      #Note: This code sample requires OpenAI Python library version 1.0.0 or higher.
 import os
 from openai import AzureOpenAI
 
+
 client = AzureOpenAI(
-  api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-  api_version="2024-02-15-preview",
-  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+  azure_endpoint = "https://ai-azureainewsletter886222063655.openai.azure.com/",
+  api_key=os.getenv("91deb199c6544add90fd043c59404168"),
+  api_version="2024-02-15-preview"
 )
 
-deployment_name = 'gpt-3.5-turbo-16k'  # This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment.
 
-# Send a completion call to generate an answer
-print('Sending a test completion job')
-start_phrase = 'Write a tagline for an ice cream shop. '
-response = client.completions.create(model=deployment_name, prompt=start_phrase, max_tokens=10)
-print(start_phrase + response.choices[0].text)
+def generate_openai_completion(prompt):
+  message_text = [
+    {"role": "system", "content": "This is a text to be summarized."},
+    {"role": "user", "content": prompt}
+  ]
+
+  completion = client.chat.completions.create(
+    model="gpt-35-turbo",
+    messages=message_text,
+    temperature=0.7,
+    max_tokens=800,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None
+  )
+  return completion.choices[0].message['content']
+
+# 
+# import os
+# from openai import AzureOpenAI
+# 
+# 
+# client = AzureOpenAI(
+#   azure_endpoint = "https://ai-azureainewsletter886222063655.openai.azure.com/",
+#   api_key=os.getenv("91deb199c6544add90fd043c59404168"),
+#   api_version="2024-02-15-preview"
+# )
+# 
+# 
+# 
+# message_text = [{"role":"system","content":"You are an AI assistant that helps people summarize an article"},{"role":"user","content":"hi how are you"},{"role":"assistant","content":"Hello! I'm an AI assistant programmed to help people summarize articles. How can I assist you today?"}]
+# 
+# completion = client.chat.completions.create(
+#   model="gpt-35-turbo", # model = "deployment_name"
+#   messages = message_text,
+#   temperature=0.7,
+#   max_tokens=800,
+#   top_p=0.95,
+#   frequency_penalty=0,
+#   presence_penalty=0,
+#   stop=None
+# )
