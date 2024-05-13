4107fd9fd9f1d514a4deb0b359e98a96d72cca28
Justyna Puz <puzjustyna@gmail.com>
2024-05-13 10:16:17 +0200

    db added

--- mining_summary/spiders/mining_summary.py
+++ mining_summary/spiders/mining_summary.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+
 import scrapy
 from scrapy.crawler import CrawlerProcess
 import os
@@ -6,15 +8,165 @@ import re
 import nltk
 from model import client  # Import klienta Azure OpenAI z model.py
 from model import generate_openai_completion
+import mysql.connector
+from mysql.connector import Error
 
 nltk.download('punkt')
+
+host_name = 'localhost'
+user_name = 'newsletter'
+user_password = 'newsletter'
+db_name = 'newsletter'
+
+
+class WhatToSave(Enum):
+    LINK = auto()
+    ARTICLE = auto()
+    SUMMARY = auto()
+    SCRAPY = auto()
+
+
+class Database:
+
+    def connect_to_db(self, host_name, user_name, user_password, db_name):
+        connection = None
+        try:
+            connection = mysql.connector.connect(
+                host=host_name,
+                user=user_name,
+                passwd=user_password,
+                database=db_name
+            )
+            print("Połączenie z bazą danych MySQL udane")
+        except Error as e:
+            print(f"Błąd '{e}' podczas połączenia z bazą danych")
+
+        return connection
+        # Połączenie z bazą danych
+
+    def insert_article(self, connection, id, link, scrapy_text):
+        cursor = connection.cursor()
+        query = "INSERT INTO article (id, link, scrapy_text) VALUES (%s, %s, %s)"
+        args = (id, link, scrapy_text)
+
+        try:
+            cursor.execute(query, args)
+            connection.commit()
+            print("Pomyślnie dodano artykuł do bazy danych")
+            return cursor.lastrowid
+        except Error as e:
+            print(f"Błąd '{e}' podczas dodawania artykułu")
+            return None
+        finally:
+            cursor.close()
+
+    def insert_link(self, connection, link):
+        cursor = connection.cursor()
+        query = "INSERT INTO article (link) VALUES (%s)"
+        args = (link,)
+
+        try:
+            cursor.execute(query, args)
+            connection.commit()
+            print("Pomyślnie dodano artykuł do bazy danych")
+            return cursor.lastrowid
+        except Error as e:
+            print(f"Błąd '{e}' podczas dodawania artykułu")
+            return None
+        finally:
+            cursor.close()
+
+    def insert_scrapy_text(self, connection, link, scrapy_text):
+        cursor = connection.cursor()
+        query = "UPDATE article SET scrapy_text = %s WHERE link = %s"
+        args = (scrapy_text, link)
+
+        try:
+            cursor.execute(query, args)
+            connection.commit()
+            print("Pomyślnie dodano artykuł do bazy danych")
+            return cursor.lastrowid
+        except Error as e:
+            print(f"Błąd '{e}' podczas dodawania artykułu")
+            return None
+        finally:
+            cursor.close()
+
+    def insert_summary(self, connection, id, category, summary):
+        cursor = connection.cursor()
+        query = "INSERT INTO article_details (article_id, category, summary) VALUES (%s, %s, %s)"
+        args = (id, category, summary)
+
+        try:
+            cursor.execute(query, args)
+            connection.commit()
+            print("Pomyślnie dodano artykuł do bazy danych")
+            return cursor.lastrowid
+        except Error as e:
+            print(f"Błąd '{e}' podczas dodawania artykułu")
+            return None
+        finally:
+            cursor.close()
+
+    def get_article_id_by_link(self, connection, link):
+        cursor = connection.cursor()
+        query = "SELECT id FROM article WHERE link = %s"
+
+        try:
+            cursor.execute(query, (link,))
+            record = cursor.fetchone()
+            print(f"ID artykułu dla linku {link} to: {record[0]}")
+            return record[0]
+        except Error as e:
+            print(f"Błąd '{e}' podczas dodawania artykułu")
+            return None
+        finally:
+            cursor.close()
+
+    def save_to_db(self, connection, parameter: WhatToSave, id, link, scrapy_text=None, summary=None, category=None):
+        if connection is not None:
+            if parameter == WhatToSave.LINK:
+                article_id = self.insert_link(connection, link)
+            elif parameter == WhatToSave.ARTICLE:
+                article_id = self.insert_article(connection, id, link, summary)
+            elif parameter == WhatToSave.SCRAPY:
+                article_id = self.insert_scrapy_text(connection, link, scrapy_text)
+            elif parameter == WhatToSave.SUMMARY:
+                article_id = self.insert_summary(connection, id, category, summary)
+
+            if article_id is not None:
+                print(f"Dodano artykuł z ID: {article_id}")
+            else:
+                print("Nie udało się dodać artykułu")
+        else:
+            print("Nie udało się połączyć z bazą danych")
+
+    # # Dane, które chcesz zapisać
+    # id =2
+    # link = "http://example.com/article"
+    # scrapy_text = "Tutaj wpisz pobrany tekst"
+
+    # Pamiętaj, aby zamknąć połączenie z bazą danych po zakończeniu
+
 
 class MiningSpider(scrapy.Spider):
     name = "mining_summary"
 
+    host_name = 'localhost'
+    user_name = 'newsletter'
+    user_password = 'newsletter'
+    db_name = 'newsletter'
+    db = Database()
+    connection = None
+
     def start_requests(self):
+        self.connection = self.db.connect_to_db(host_name, user_name, user_password, db_name)
         with open('links.txt', 'r') as file:
             start_urls = [url.strip() for url in file.readlines() if url.strip()]
+            print(f"URLs loaded: {start_urls}")
+
+        for url in start_urls:
+            self.db.save_to_db(self.connection, WhatToSave.LINK, 1, url)
 
         for url in start_urls:
             yield scrapy.Request(url, self.parse)
@@ -34,6 +186,9 @@ class MiningSpider(scrapy.Spider):
         content = ' '.join(page_content)
         cleaned_content = self.clean_text(content)
 
+        # Save scrapy text to db
+        self.db.save_to_db(self.connection, WhatToSave.SCRAPY, 9, response.url, cleaned_content)
+
         # Save original content to a file
         raw_filename = f"{response.url.split('/')[-2]}_raw.txt"
         with open(raw_filename, 'w', encoding='utf-8') as f:
@@ -41,6 +196,16 @@ class MiningSpider(scrapy.Spider):
 
         # Generate summary from the cleaned content
         summary = self.generate_summary(cleaned_content)
+
+        # get article id
+        article_id = self.db.get_article_id_by_link(self.connection, response.url)
+        category = "None"
+
+        if article_id:
+            print(f"Znaleziono ID: {article_id}")
+
+        # Save summary to db
+        self.db.save_to_db(self.connection, WhatToSave.SUMMARY, article_id, None, None, summary, category)
 
         # Save summary to a file
         summary_filename = f"{response.url.split('/')[-2]}_summary.txt"
@@ -57,64 +222,13 @@ class MiningSpider(scrapy.Spider):
         prompt = "Summarize this text in max 4 sentences: " + text
         return generate_openai_completion(prompt)
 
+    def closed(self):
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+            print("Połączenie z bazą danych zostało zamknięte")
+
+
 if __name__ == "__main__":
     process = CrawlerProcess()
     process.crawl(MiningSpider)
     process.start()
-# 
-# import scrapy
-# from scrapy.crawler import CrawlerProcess
-# from sumy.parsers.plaintext import PlaintextParser
-# from sumy.nlp.tokenizers import Tokenizer
-# from sumy.summarizers.lsa import LsaSummarizer
-# import nltk
-# import html
-# import re
-# 
-# nltk.download('punkt')
-# 
-# from scrapy.utils.project import get_project_settings  # Import do użycia ustawień projektu
-# class MiningSpider(scrapy.Spider):
-#     name = "mining_summary"
-# 
-#     def start_requests(self):
-#         # Otwórz plik 'links.txt' i wczytaj URL-e
-#         with open('links.txt', 'r') as file:
-#             links = [url.strip() for url in file.readlines()]
-#             for url in links:
-#                 yield scrapy.Request(url=url, callback=self.parse)
-# 
-#                 
-#     def parse(self, response):
-#             self.logger.info(f"Przetwarzanie: {response.url}")
-#             if "koreabizwire.com" in response.url:
-#                 page_content = response.xpath('//div[@class="entry-content"]//p//text()').getall()
-#             else:
-#                 page_content = response.xpath('//p//text()').getall()
-#             if not page_content:
-#                 self.logger.error(f"Nie wyekstrahowano treści z: {response.url}")
-#             else:
-#                 self.logger.info(f"Ekstrahowana treść: {page_content[:100]}")
-#             content = ' '.join(page_content)
-#             cleaned_content = self.clean_text(content)
-#             summary = self.generate_summary(cleaned_content)
-#             filename = f"{response.url.split('/')[-2]}.txt"
-#             with open(filename, 'w', encoding='utf-8') as f:
-#                 f.write(summary)
-# 
-#     def clean_text(self, text):
-#             text = html.unescape(text)  # Dekoduje encje HTML
-#             text = re.sub(r'\s+', ' ', text)  # Usuwa nadmiarowe białe znaki
-#             return text.strip()
-# 
-#     def generate_summary(self, text, sentences_count=3):
-#             parser = PlaintextParser.from_string(text, Tokenizer("english"))
-#             summarizer = LsaSummarizer()
-#             summary = summarizer(parser.document, sentences_count)
-#             return ' '.join([str(sentence) for sentence in summary])
-# 
-#     # Uruchomienie pająka (dla celów przykładowych, zazwyczaj używa się 'scrapy crawl' z terminala)
-# if __name__ == "__main__":
-#     process = CrawlerProcess()
-#     process.crawl(MiningSpider)
-#     process.start()
--- mining_summary/spiders/model.py
+++ mining_summary/spiders/model.py
@@ -1,56 +1,30 @@
-#Note: The openai-python library support for Azure OpenAI is in preview.
-      #Note: This code sample requires OpenAI Python library version 1.0.0 or higher.
+# Note: The openai-python library support for Azure OpenAI is in preview.
+# Note: This code sample requires OpenAI Python library version 1.0.0 or higher.
 import os
 from openai import AzureOpenAI
 
 
 client = AzureOpenAI(
-  azure_endpoint = "https://ai-azureainewsletter886222063655.openai.azure.com/",
-  api_key=os.getenv("91deb199c6544add90fd043c59404168"),
-  api_version="2024-02-15-preview"
+  azure_endpoint = "https://newsai.openai.azure.com/",
+  api_key = "8d131e954ff74d7696129872388aee0c",
+  api_version = "2024-02-15-preview"
 )
 
-
 def generate_openai_completion(prompt):
-  message_text = [
-    {"role": "system", "content": "This is a text to be summarized."},
-    {"role": "user", "content": prompt}
-  ]
+    message_text = [
+        {"role": "system", "content": "This is a text to be summarized."},
+        {"role": "user", "content": prompt}
+    ]
 
-  completion = client.chat.completions.create(
-    model="gpt-35-turbo",
-    messages=message_text,
-    temperature=0.7,
-    max_tokens=800,
-    top_p=0.95,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=None
-  )
-  return completion.choices[0].message.content
 
-# 
-# import os
-# from openai import AzureOpenAI
-# 
-# 
-# client = AzureOpenAI(
-#   azure_endpoint = "https://ai-azureainewsletter886222063655.openai.azure.com/",
-#   api_key=os.getenv("91deb199c6544add90fd043c59404168"),
-#   api_version="2024-02-15-preview"
-# )
-# 
-# 
-# 
-# message_text = [{"role":"system","content":"You are an AI assistant that helps people summarize an article"},{"role":"user","content":"hi how are you"},{"role":"assistant","content":"Hello! I'm an AI assistant programmed to help people summarize articles. How can I assist you today?"}]
-# 
-# completion = client.chat.completions.create(
-#   model="gpt-35-turbo", # model = "deployment_name"
-#   messages = message_text,
-#   temperature=0.7,
-#   max_tokens=800,
-#   top_p=0.95,
-#   frequency_penalty=0,
-#   presence_penalty=0,
-#   stop=None
-# )
+    completion = client.chat.completions.create(
+        model="gpt-35-turbo",  # model = "deployment_name"
+        messages=message_text,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    return completion.choices[0].message.content
